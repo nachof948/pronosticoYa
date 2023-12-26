3df6d44eeea5d0cd792f9823e92eb5d1bec59de1
nachof948 <nachof948@gmail.com>
2023-12-26 09:10:58 -0300

    Rutas del frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React  from 'react';
 import { Home } from './indice';
-
+import { Routes, Route } from 'react-router-dom'
 
 function App() {  
   return (
     <body className="App">
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/auth/registrarse'></Route>
+        <Route path='/auth/iniciar-sesion'></Route>
+      </Routes>
     </body>
   );
 }
